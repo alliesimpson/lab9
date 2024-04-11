42adb01586fa3c9b6b838696a00079ae7c186b58
Allie Simpson <alliesimpson@ufl.edu>
2024-04-11 11:54:37 -0400

    Encoder and Main

--- main.py
+++ main.py
@@ -1,3 +1,32 @@
 #This is my first github addition
 print ( 'hello world')
 
+#Partners name: Zitlaly
+def menu():
+	print("Menu")
+	print("-------------")
+	print("1. Encode")
+	print("2. Decode")
+	print("3. Quit")
+
+def encode(password):
+	new_password = ""
+	for  number in password:
+		number = int(number) + 3
+		new_password += str(number)
+	return new_password
+
+def main():
+	while True:
+		print()
+		menu()
+		choice = int(input("Please enter your password to encode: ")
+		if choice == 1:
+			password = input("Please enter your password to encode: ")
+			encode(password)
+			print("Your password has been encoded and stored!")
+		elif choice == 3:
+			break
+
+if __name__ == "__main__":
+	main()
